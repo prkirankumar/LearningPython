5ee707f5cfa24772778b854cb69113ad1b313ac8
prkirankumar <reddykkp@hotmail.com>
2020-03-30 23:45:19 +0530

    examples of for

---
+++ Basics/conditional_for.py
@@ -0,0 +1,19 @@
+x = [10, 12, 13, 14, 17, 19, 21, 22, 25]
+
+for i in x:
+    print(i)
+
+for i in x:
+    print(i%3)
+
+for i in sorted(x,reverse=True):
+    print(i)
+
+for i in x:
+    print(i / 2)
+else:
+    print("Great job!")
+
+for i in x:
+    print(x.index(i))
+
